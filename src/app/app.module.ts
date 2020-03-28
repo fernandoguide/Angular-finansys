4f6baf0082e65105567a1aec652a87fac49ebe48
fernandoguide <fernando-guide@hotmail.com>
2020-03-28 12:33:36 -0300

    buscando a lista das category do inMemoryDb e conseguindo excluir um item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { InMemoryDatabase } from "./in-memory-database";
 import { CategoriesModule } from './pages/categories/categories.module';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,9 @@ import { CategoriesModule } from './pages/categories/categories.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     CategoriesModule,
+     // para interceptar requisicao do inMemoryDataBase quando for usar api externa remover
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
   ],
   providers: [],
--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -3,20 +3,19 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 
 
 export class InMemoryDatabase implements InMemoryDbService {
-  createDb(){
+  createDb() {
     const categories: Category[] = [
       { id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa' },
       { id: 2, name: 'Saúde', description: 'Plano de Saúde e Remédios' },
       { id: 3, name: 'Faculdade', description: 'Faculdade BandTec' },
-      { id: 4, name: 'Comida', description: 'Almoço e Janta' },
+      { id: 4, name: 'Comidas', description: 'Almoço e Jantar' },
       { id: 5, name: 'VT', description: 'Vale Transporte' },
       { id: 6, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
-      { id: 7, name: 'Salário', description: 'Recebimento de Salário'},
-      { id: 8, name: 'Freelas', description: 'Trabalhos como freelancer'},
-      { id: 9, name: 'VA', description: 'Vale Alimentação'},
-      { id: 10, name: 'VC', description: 'Vale Combustivel'},
-      { id: 11, name: 'Financiamento Veiculo', description: 'Financiamento do carro'},
-      { id: 12, name: 'Viagens', description: 'Gastos com viagens e passagens.'}
+      { id: 7, name: 'Salário', description: 'Recebimento de Salário' },
+      { id: 8, name: 'Freelas', description: 'Trabalhos como freelancer' },
+      { id: 9, name: 'VA', description: 'Vale Alimentação' },
+      { id: 10, name: 'VC', description: 'Vale Combustivel' },
+      { id: 11, name: 'Financiamento Veiculo', description: 'Financiamento do carro' }
     ];
 
     return { categories };
